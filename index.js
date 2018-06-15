33f75ff4e25c872eb0f62bc4c7f2b3510e906788
Julien Bérubé <julien.berube@gmail.com>
2018-06-15 16:39:51 -0400

    fixed index.js and transports/stream.js imports fixed level being written twice in StreamTransport

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-export * from './lib/core';
-export * from './lib/logger'
-export * from './lib/transports';
+module.exports = require('./lib/core');
+
+const transports = require('./lib/transports');
+Object.entries(transports).forEach(entry => {
+    module.exports[entry[0]] = entry[1];
+});
+
+module.exports.Ariane = module.exports.Logger = require('./lib/logger');
